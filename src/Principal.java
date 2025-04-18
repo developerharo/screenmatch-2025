6ca324a2ff78e50baab0a67acd099dfac128adea
developerharo <developerharo@gmail.com>
2025-04-17 23:34:25 -0600

    Agregando Principal.java (Para instanciar la clase modelo Pelicula)

---
+++ src/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Titanic";
+        miPelicula.fechaDeLanzamiento = 1997;
+        miPelicula.duracionEnMinutos = 120;
+
+        System.out.println("Película: " + miPelicula.nombre);
+        System.out.println("Año de estreno: " + miPelicula.fechaDeLanzamiento);
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1998;
+        otraPelicula.duracionEnMinutos = 110;
+
+        System.out.println("Película: " + otraPelicula.nombre);
+        System.out.println("Año de estreno: " + otraPelicula.fechaDeLanzamiento);
+
+
+    }
+}
